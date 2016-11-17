530b8597233af7205dd394be93785d0100001ccb
iny7 <qinchangxi@gmail.com>
2016-11-18 00:47:53 +0800

    ADD: bootstrap layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,24 @@ class ApplicationController < ActionController::Base
       @current_user ||= cookies[:auth_token] && User.find_by_auth_token(cookies[:auth_token])
     end
 
+    def logged_in?
+      !!current_user
+    end
+
+    def login_required
+      logged_in? || access_denied
+    end
+
+    def access_denied
+      respond_to do |wants|
+        wants.html do
+          session[:return_to] = request.fullpath
+          redirect_to signin_url
+        end
+        wants.json { render :json => "Access denied.", :status => '403'}
+      end
+    end
+
   helper_method :current_user
 
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,8 @@
 class WelcomeController < ApplicationController
 
+  before_action :login_required, only: [:index]
   # wechat_api
   def index
-    redirect_to '/signin'
+    redirect_to articles_url
   end
 end
